3b8f991534f197050692482a31e28896f9ed6c98
sumanth <sumanth@pallamreddy.com>
2023-01-18 23:29:31 -0800

    extending sync?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ExtenderTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ExtenderTesting.java
@@ -13,32 +13,54 @@ import org.firstinspires.ftc.teamcode.Killabytez.Elevator;
 public class ExtenderTesting extends LinearOpMode {
     public static int lifting = 500;
     public static double pow = 0.4;
+    DcMotor leftExtender= hardwareMap.get(DcMotor.class, "leftextender");;
+    DcMotor rightExtender=hardwareMap.get(DcMotor.class, "rightextender");
+    Thread leftExtending=new Thread (new Runnable() {
+        public void run() {
+            leftExtender.setTargetPosition(lifting);
+            leftExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftExtender.setPower(pow);
+        }
+    });
+    Thread rightExtending=new Thread (new Runnable() {
+        public void run() {
+            rightExtender.setTargetPosition(lifting);
+            rightExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightExtender.setPower(pow);
+        }
+    });
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor leftExtender;
-        DcMotor rightExtender=hardwareMap.get(DcMotor.class, "rightextender");
-        leftExtender = hardwareMap.get(DcMotor.class, "leftextender");
 
         //2400 ticks to top
         //1100 ticks to mid
-        leftExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
 
         leftExtender.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightExtender.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftExtender.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightExtender.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        leftExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart();
-        leftExtender.setTargetPosition(lifting + leftExtender.getCurrentPosition());
+        /*leftExtender.setTargetPosition(lifting + leftExtender.getCurrentPosition());
         rightExtender.setTargetPosition(lifting + rightExtender.getCurrentPosition());
         leftExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftExtender.setPower(pow);
         rightExtender.setPower(pow);
+*/
+        //sync leftExtending and rightExtending threads
+        leftExtending.join();
+        rightExtending.join();
+        leftExtending.start();
+        rightExtending.start();
+        sleep(500);
+        leftExtender.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightExtender.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        while(leftExtender.isBusy() && rightExtender.isBusy()) {
+        while(leftExtender.isBusy() || rightExtender.isBusy()) {
 
 
 
@@ -49,9 +71,10 @@ public class ExtenderTesting extends LinearOpMode {
         leftExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         telemetry.addData("hello", leftExtender.getCurrentPosition());
+        telemetry.addData("hell", rightExtender.getCurrentPosition());
         telemetry.update();
 
-        sleep(1000);
+        /*sleep(1000);
         leftExtender.setTargetPosition(0);
         rightExtender.setTargetPosition(0);
         leftExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -66,7 +89,7 @@ public class ExtenderTesting extends LinearOpMode {
         leftExtender.setPower(0);
         rightExtender.setPower(0);
         leftExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);*/
     }
 
 
